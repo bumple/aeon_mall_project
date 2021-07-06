1c70172bf933b95d9224953f269d7815f5544e66
thanhdeptrai <thanh.n5151@gmail.com>
2021-07-06 15:22:06 +0700

    thanh

--- resources/views/core/header.blade.php
+++ resources/views/core/header.blade.php
@@ -101,4 +101,80 @@
         </div>
     </div>
 </div>
+
+<!-- Modal-->
+<!-- medium modal -->
+{{--<div class="modal fade" id="mediumModal" tabindex="-1" role="dialog" aria-labelledby="mediumModalLabel"--}}
+{{--     aria-hidden="true">--}}
+{{--    <div class="modal-dialog" role="document">--}}
+{{--        <div class="modal-content">--}}
+{{--            <div class="modal-header">--}}
+{{--                <button type="button" class="close" data-dismiss="modal" aria-label="Close">--}}
+{{--                    <span aria-hidden="true">&times;</span>--}}
+{{--                </button>--}}
+{{--            </div>--}}
+{{--            <div class="modal-body" id="mediumBody">--}}
+{{--                <div>--}}
+{{--                    <!-- the result to be displayed apply here -->--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</div>--}}
+
+
+{{--<script>--}}
+{{--    // display a modal (small modal)--}}
+{{--    $(document).on('click', '#smallButton', function(event) {--}}
+{{--        event.preventDefault();--}}
+{{--        let href = $(this).attr('data-attr');--}}
+{{--        $.ajax({--}}
+{{--            url: href,--}}
+{{--            beforeSend: function() {--}}
+{{--                $('#loader').show();--}}
+{{--            },--}}
+{{--            // return the result--}}
+{{--            success: function(result) {--}}
+{{--                $('#smallModal').modal("show");--}}
+{{--                $('#smallBody').html(result).show();--}}
+{{--            },--}}
+{{--            complete: function() {--}}
+{{--                $('#loader').hide();--}}
+{{--            },--}}
+{{--            error: function(jqXHR, testStatus, error) {--}}
+{{--                console.log(error);--}}
+{{--                alert("Page " + href + " cannot open. Error:" + error);--}}
+{{--                $('#loader').hide();--}}
+{{--            },--}}
+{{--            timeout: 8000--}}
+{{--        })--}}
+{{--    });--}}
+
+{{--    // display a modal (medium modal)--}}
+{{--    $(document).on('click', '#mediumButton', function(event) {--}}
+{{--        event.preventDefault();--}}
+{{--        let href = $(this).attr('data-attr');--}}
+{{--        $.ajax({--}}
+{{--            url: href,--}}
+{{--            beforeSend: function() {--}}
+{{--                $('#loader').show();--}}
+{{--            },--}}
+{{--            // return the result--}}
+{{--            success: function(result) {--}}
+{{--                $('#mediumModal').modal("show");--}}
+{{--                $('#mediumBody').html(result).show();--}}
+{{--            },--}}
+{{--            complete: function() {--}}
+{{--                $('#loader').hide();--}}
+{{--            },--}}
+{{--            error: function(jqXHR, testStatus, error) {--}}
+{{--                console.log(error);--}}
+{{--                alert("Page " + href + " cannot open. Error:" + error);--}}
+{{--                $('#loader').hide();--}}
+{{--            },--}}
+{{--            timeout: 8000--}}
+{{--        })--}}
+{{--    });--}}
+
+{{--</script>--}}
 <!-- End Header -->
